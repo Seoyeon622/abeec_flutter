73c26b2696363ba152c4fbd6658f0d141f45a83e
yoojin Jang <yoojinjangjang@gmail.com>
2022-04-14 20:45:29 +0900

    unity

--- lib/screens/MainPage.dart
+++ lib/screens/MainPage.dart
@@ -1,12 +1,15 @@
 import 'package:capstone_abeec/screens/MyVocaList.dart';
 import 'package:capstone_abeec/screens/SearchVoca.dart';
+import 'package:capstone_abeec/screens/unity.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:get/get.dart';
 
 class MainPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
     return Scaffold(
       appBar: AppBar(
         title: Text("메인 화면"),
@@ -21,6 +24,9 @@ class MainPage extends StatelessWidget {
             ElevatedButton(
                 onPressed: () {Get.to(MyVoca());},
                 child: Text("내 단어장")),
+            ElevatedButton(
+                onPressed: () {Get.to(UnityDemoScreen());},
+                child: Text("미니게임")),
           ],
         ),
       ),
